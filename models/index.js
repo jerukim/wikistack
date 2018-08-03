5b1dc8dafac4537b20aaed236d4ae3175f113b38
tomasgnr <tomasgnr@gmail.com>
2018-08-03 11:08:13 -0500

    fixing gitignore

--- models/index.js
+++ models/index.js
@@ -4,3 +4,18 @@ const db = new Sequelize('postgres://localhost:5432/wikistack');
 module.exports = {
   db
 }
+
+//DB Model Creation
+const Page = db.define('page', {
+  title: Sequilize.STRING,
+  slug: Sequilize.STRING,
+  content: Sequilize.TEXT,
+  status: {
+    type: Sequilize.ENUM('open', 'closed')
+  }
+});
+
+const User = db.define('user', {
+  name: Sequilize.STRING,
+  email: Sequilize.STRING,
+});
