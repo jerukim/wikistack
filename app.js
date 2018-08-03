c16e1d363ab62738d2ace68f44b8167100d4c865
Jeru Kim <mikurej95@gmail.com>
2018-08-03 10:27:40 -0500

    setup complete

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
-const static = express.static();
+// const static = express.static();
+const layout = require('./views/layout');
 
 const app = express();
 
 app.use(express.urlencoded({extended: false}));
 app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/stylesheets'));
 
+
+
+app.get('/', (req, res) => {
+  res.send(layout(''));
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`app listening on port ${PORT}`);
+});
